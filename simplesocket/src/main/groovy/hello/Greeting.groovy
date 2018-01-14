00c49620501c1cb544b6e334b799600c13343c0e
MarkKlimenko <22773@bk.ru>
2018-01-14 15:28:39 +1000

    Add simple socket application

---
+++ simplesocket/src/main/groovy/hello/Greeting.groovy
@@ -0,0 +1,18 @@
+package hello
+
+class Greeting {
+
+    private String content
+
+    Greeting() {
+    }
+
+    Greeting(String content) {
+        this.content = content
+    }
+
+    String getContent() {
+        return content
+    }
+
+}
