e2f23a6496296e7942c26775af7bd7b1c1c89c14
MarkKlimenko <22773@bk.ru>
2018-01-21 22:18:21 +1000

    Rewrite AggregateActor in groovy style

--- essentials/src/main/groovy/systems/vostok/enzo/essentials/mapreduce/actors/AggregateActor.groovy
+++ essentials/src/main/groovy/systems/vostok/enzo/essentials/mapreduce/actors/AggregateActor.groovy
@@ -1,36 +1,28 @@
 package systems.vostok.enzo.essentials.mapreduce.actors
 
+import akka.actor.AbstractActor
 import akka.actor.ActorRef
-import akka.actor.UntypedActor
 import systems.vostok.enzo.essentials.mapreduce.messages.ReduceData
 import systems.vostok.enzo.essentials.mapreduce.messages.Result
 
-class AggregateActor extends UntypedActor {
-    private Map<String, Integer> finalReducedMap =
-            new HashMap<String, Integer>()
+class AggregateActor extends AbstractActor {
+    private Map<String, Integer> finalReducedMap = new HashMap<>()
 
     @Override
-    void onReceive(Object message) throws Exception {
-        if (message instanceof ReduceData) {
-            ReduceData reduceData = (ReduceData) message
-
-            aggregateInMemoryReduce(reduceData.
-                    getReduceDataList())
-        } else if (message instanceof Result) {
-            getSender().tell(finalReducedMap.toString(), ActorRef.noSender())
-        } else
-            unhandled(message)
+    Receive createReceive() {
+        receiveBuilder()
+                .match(ReduceData.class, { aggregateInMemoryReduce(it.reduceDataList) })
+                .match(Result.class, { getSender().tell(finalReducedMap.toString(), ActorRef.noSender()) })
+                .matchAny({ unhandled(it) })
+                .build()
     }
-    private void aggregateInMemoryReduce(Map<String,
-            Integer> reducedList) {
-        Integer count = null
-        for (String key : reducedList.keySet()) {
+
+    private void aggregateInMemoryReduce(Map<String, Integer> reducedList) {
+        reducedList.each { key, value ->
             if (finalReducedMap.containsKey(key)) {
-                count = reducedList.get(key) +
-                        finalReducedMap.get(key)
-                finalReducedMap.put(key, count)
+                finalReducedMap[key] = reducedList[key] + finalReducedMap[key]
             } else {
-                finalReducedMap.put(key, reducedList.get(key))
+                finalReducedMap[key] = reducedList[key]
             }
         }
     }
